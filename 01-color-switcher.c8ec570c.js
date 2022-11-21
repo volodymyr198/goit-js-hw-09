d0d181e9e2df786b10cf073c6cc6de55b8185ab1
volodymyr198 <107930588+volodymyr198@users.noreply.github.com>
2022-11-21 11:52:24 +0000

    Deploying to gh-pages from @ volodymyr198/goit-js-hw-09@fad035285c75b16d724f63e9f4952ef7c1873c3e 🚀

---
+++ 01-color-switcher.c8ec570c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),d=2;n.disabled=!0;e.addEventListener("click",(function(t){setInterval(o,1e3),n.disabled=!1,e.disabled=!0}));function o(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}n.addEventListener("click",(function(t){clearInterval(d),d+=1,n.disabled=!0,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.c8ec570c.js.map
