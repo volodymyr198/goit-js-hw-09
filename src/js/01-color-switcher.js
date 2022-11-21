fad035285c75b16d724f63e9f4952ef7c1873c3e
volodymyr198 <volodymyr.b.198@gmail.com>
2022-11-21 13:51:55 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyEl = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let id = 2;
 
+btnStop.disabled = true;
+
+const onPressStart = event => {
+  setInterval(startBackgroundColor, 1000);
+
+  btnStop.disabled = false;
+  btnStart.disabled = true;
+};
+btnStart.addEventListener('click', onPressStart);
+
+const onPressStop = event => {
+  clearInterval(id);
+  id += 1;
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+};
+btnStop.addEventListener('click', onPressStop);
+
+function startBackgroundColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
