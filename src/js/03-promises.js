47ec4421f249bdaca6cf0223d21333a573ba26b2
volodymyr198 <volodymyr.b.198@gmail.com>
2022-11-23 16:08:46 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  formEl: document.querySelector('form'),
+};
+
+const onCreatePromises = event => {
+  event.preventDefault();
+
+  let delay = Number(refs.formEl[0].value);
+  const step = Number(refs.formEl[1].value);
+  const amount = Number(refs.formEl[2].value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay).then(onSucces).catch(onError);
+    delay += step;
   }
+};
+refs.formEl.addEventListener('submit', onCreatePromises);
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+
+      reject({ position, delay });
+    }, delay);
+  });
+}
+
+function onSucces({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
